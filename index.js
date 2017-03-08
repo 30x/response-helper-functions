b5c91d3dc947c0088456f077a00f745ce0dbf9ce
Martin Nally <mnally@google.com>
2017-03-08 13:58:04 -0800

    fix uuidw bug

--- index.js
+++ index.js
@@ -172,11 +172,10 @@ function uuid4() {
 
 const words = fs.readFileSync(path.join(__dirname, '65536words'), 'utf-8').split('\n')
 function uuidw() {
-  var buf = randomBytes(16)
-  var rslt = ''
+  var buf = randomBytes(16), rslt = ''
   for (var i = 0; i < 12; i++)
     rslt += toHex[buf[i]]
-  rslt +=  '-' + words[buf[0] * 256 + buf[1]] + '-' + words[buf[14] * 256 + buf[15]]  
+  rslt +=  '-' + words[buf[12] * 256 + buf[13]] + '-' + words[buf[14] * 256 + buf[15]]  
   return rslt
 }
 
